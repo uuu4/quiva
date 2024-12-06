7ffc395b2b8fedf3c8538b1a93e8c7b9f354d18a
Ali Emre Aydın <23120205056@ismu.edu.tr>
2024-12-06 21:36:48 +0300

    updated logging.c
    added timer functions

--- src/utils/logging.c
+++ src/utils/logging.c
@@ -1,3 +1,15 @@
-//
-// Created by Ali Emre Aydin on 4.12.2024.
-//
+#include <time.h>
+#include "utils.h"
+#include <stdio.h>
+
+
+static clock_t start;
+void start_timer() {
+    start = clock();
+}
+
+double stop_timer() {
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    return time_spent;
+}
