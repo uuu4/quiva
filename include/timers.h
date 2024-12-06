80f19be58e437aa43fb16e2b93f9ad7dd5248ca4
Ali Emre Aydın <23120205056@ismu.edu.tr>
2024-12-07 01:18:27 +0300

    updated timers.h

--- include/timers.h
+++ include/timers.h
@@ -4,10 +4,17 @@
 
 #ifndef TIMERS_H
 #define TIMERS_H
+#include <time.h>
+#include <stdio.h>
+typedef struct Timer {
+    clock_t start_time;
+    clock_t end_time;
+} Timer;
+
 
 //time tracking(for performance testing)
 void start_timer();
 double stop_timer();
 
-
+void log_timer_duration(Timer* timer, const char* label);
 #endif //TIMERS_H
