b2b42495fc607fceeb7ea8d5459a6e99295b075d
Ali Emre Aydın <23120205056@ismu.edu.tr>
2024-12-11 16:07:26 +0300

    added pauli z gate

--- src/core/gate.c
+++ src/core/gate.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 
-Qubit* apply_pauli_y_gate(Qubit* qb) {
+Qubit* apply_pauli_x_gate(Qubit* qb) {
     double** pauli_x = (double**)malloc(2 * sizeof(double*));
     for (int i = 0; i < 2; i++) {
         pauli_x[i] = (double*)malloc(2 * sizeof(double));
@@ -55,22 +55,22 @@ Qubit* apply_pauli_y_gate(Qubit* qb) {
     return result_qb;
 }
 
-Qubit* apply_pauli_x_gate(Qubit* qb) {
-    double** pauli_x = (double**)malloc(2 * sizeof(double*));
+Qubit* apply_pauli_z_gate(Qubit* qb) {
+    double** pauli_z = (double**)malloc(2 * sizeof(double*));
     for (int i = 0; i < 2; i++) {
-        pauli_x[i] = (double*)malloc(2 * sizeof(double));
+        pauli_z[i] = (double*)malloc(2 * sizeof(double));
     }
-    pauli_x[0][0] = 0; pauli_x[0][1] = 1;
-    pauli_x[1][0] = 1; pauli_x[1][1] = 0;
+    pauli_z[0][0] = 1; pauli_z[0][1] = 0;
+    pauli_z[1][0] = 0; pauli_z[1][1] = -1;
 
     double** qb_matrix = qubit_to_matrix(qb);
 
-    double** matrix_result = matrix_multiply(pauli_x, qb_matrix, 2, 2, 2, 1);
+    double** matrix_result = matrix_multiply(pauli_z, qb_matrix, 2, 2, 2, 1);
     if (matrix_result == NULL) {
         for (int i = 0; i < 2; i++) {
-            free(pauli_x[i]);
+            free(pauli_z[i]);
         }
-        free(pauli_x);
+        free(pauli_z);
         free(qb_matrix);
         return NULL;
     }
@@ -78,11 +78,11 @@ Qubit* apply_pauli_x_gate(Qubit* qb) {
     Qubit* result_qb = (Qubit*)malloc(sizeof(Qubit));
     if (result_qb == NULL) {
         for (int i = 0; i < 2; i++) {
-            free(pauli_x[i]);
+            free(pauli_z[i]);
             free(qb_matrix[i]);
             free(matrix_result[i]);
         }
-        free(pauli_x);
+        free(pauli_z);
         free(qb_matrix);
         free(matrix_result);
         return NULL;
@@ -94,11 +94,11 @@ Qubit* apply_pauli_x_gate(Qubit* qb) {
     result_qb->imag[1] = matrix_result[1][1];
 
     for (int i = 0; i < 2; i++) {
-        free(pauli_x[i]);
+        free(pauli_z[i]);
         free(qb_matrix[i]);
         free(matrix_result[i]);
     }
-    free(pauli_x);
+    free(pauli_z);
     free(qb_matrix);
     free(matrix_result);
 
