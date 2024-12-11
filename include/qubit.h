0f85447f349ab278542719d0005a760e749a5115
Ali Emre Aydın <23120205056@ismu.edu.tr>
2024-12-11 16:01:29 +0300

    updated qubit.h

--- include/qubit.h
+++ include/qubit.h
@@ -2,14 +2,14 @@
 #define QUBIT_H
 #include <stdbool.h>
 
-// Qubit structure definition
+// qubit structure definition
 typedef struct {
     double real[2];
     double imag[2];
 } Qubit;
 
 
-void init_qubit(Qubit* qb); // Initializes a qubit to |0⟩
+void init_qubit(Qubit* qb); // initializes a qubit to |0⟩
 void init_custom_qubit(Qubit* qb, double real0, double imag0, double real1, double imag1);
 void print_qubit(const Qubit* qb);
 void normalize_qubit(Qubit* qb);
