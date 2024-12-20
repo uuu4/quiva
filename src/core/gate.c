9d0500bbdd0c4b26b12dafd29f98badd1fa87381
Ali Emre Aydın <23120205056@ismu.edu.tr>
2024-12-20 19:49:16 +0300

    Update gate.c

--- src/core/gate.c
+++ src/core/gate.c
@@ -177,7 +177,7 @@ Qubit* apply_rotation_gate(Qubit* qb, char axis, double theta) {
         result_qb->imag[0] = qb->real[0] * sin(theta / 2) + qb->imag[0] * cos(theta / 2);
         result_qb->real[1] = qb->real[1] * cos(theta / 2) - qb->imag[1] * sin(theta / 2);
         result_qb->imag[1] = qb->real[1] * sin(theta / 2) + qb->imag[1] * cos(theta / 2);
-        break; // gotta check if these are correct NOT WORKING CURRENTLY
+        break;
     }
     return result_qb;
 }
